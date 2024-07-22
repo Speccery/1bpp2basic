280e89b52ff9016ca66ed43479ffaa3ceb527360
Erik Piehl <epiehl@gmail.com>
2024-07-22 19:45:33 +0300

    changed the size to 16k

--- compile_config.sh
+++ compile_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 xga99.py --aorg 0x6000 config.gpl -L config.lst -o config.bin
 dd if=/dev/zero bs=1 count=$(expr 8192 - $(stat -f "%Dz" config.bin))  >> config.bin
-# Add the firmware into the next 24K
-dd if=../STM32EP/grommy2/Release/grommy2.bin bs=1 count=24K >> config.bin
+# Add the firmware into the next 16K
+dd if=../STM32EP/grommy2/Release/grommy2.bin bs=1 count=16K >> config.bin
